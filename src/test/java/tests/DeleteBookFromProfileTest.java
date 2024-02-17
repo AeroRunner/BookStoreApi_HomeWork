b22c20f8a20834d48950d86b02779bbc9ae29a4f
AeroRunner <airplay1x6@gmail.com>
2024-02-17 20:54:52 +0300

    fix3

--- src/test/java/tests/DeleteBookFromProfileTest.java
+++ src/test/java/tests/DeleteBookFromProfileTest.java
@@ -15,44 +15,44 @@ import static io.qameta.allure.Allure.step;
 public class DeleteBookFromProfileTest extends BaseTest {
     @Test
     @WithSession
-    void deleteBookFromProfileTest () {
-        LoginResponseModel authResponse = step("Add Authorization Request", ()->
+    void deleteBookFromProfileTest() {
+        LoginResponseModel authResponse = step("Add Authorization Request", () ->
                 AuthorizationApi.authResponse()
         );
 
-        step("Delete all books from Profile", ()->
+        step("Delete all books from Profile", () ->
                 BooksApi.deleteAllBooks(authResponse.getToken(), authResponse.getUserId())
         );
 
-        step("Add new book to Profile", ()->
+        step("Add new book to Profile", () ->
                 BooksApi.addBooks(authResponse.getToken(), authResponse.getUserId())
         );
         step("Accept cookies", () -> {
-            if ($(".fc-consent-root").isDisplayed()){
-                $("button.fc-cta-consent").click();
-            }});
-
-        step("Open Profile",  ()->
-                open("/profile")
-        );
-        step("Accept cookies", () -> {
-            if ($(".fc-consent-root").isDisplayed()){
+            if ($(".fc-consent-root").isDisplayed()) {
                 $("button.fc-cta-consent").click();
             }
         });
-        step("Click trash icon ",  ()-> {
+
+        step("Open Profile", () ->
+                open("/profile")
+        );
+        step("Accept cookies", () -> {
+            if ($(".fc-consent-root").isDisplayed()) {
+                $("button.fc-cta-consent").click();
+            }
+        });
+        step("Click trash icon ", () -> {
             executeJavaScript("$('.fc-dialog-container').remove()");
             $("#delete-record-undefined").click();
         });
 
 
-        step("Agree with the removal of book",  ()->
+        step("Agree with the removal of book", () ->
                 $("#closeSmallModal-ok").click()
         );
 
-        step("Let's check that book has been deleted",  ()->
-                $(".rt-noData").shouldBe(visible)
-                        .shouldHave(text("No rows found"))
+        step("Let's check that book has been deleted", () ->
+                $(".rt-noData").shouldBe(visible).shouldHave(text("No rows found"))
         );
     }
 }
