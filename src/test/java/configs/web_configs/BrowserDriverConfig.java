b8c45cbed96f6f2e6eb1eb753ca5ef988e153499
AeroRunner <airplay1x6@gmail.com>
2024-02-17 19:50:35 +0300

    fix

--- src/test/java/configs/web_configs/BrowserDriverConfig.java
+++ src/test/java/configs/web_configs/BrowserDriverConfig.java
@@ -2,17 +2,22 @@ package configs.web_configs;
 
 import org.aeonbits.owner.Config;
 
-@Config.Sources({"classpath:config/${host}.properties"})
+@Config.Sources({"classpath:properties/${host}.properties"})
+
 public interface BrowserDriverConfig extends Config {
+
     @Key("browser")
     @DefaultValue("CHROME")
     String browserName();
+
     @Key("version")
     @DefaultValue("100")
     String browserVersion();
+
     @Key("remoteWebDriver")
     @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub/")
     String remoteWebDriver();
+
     @Key("browserSize")
     @DefaultValue("1920x1080")
     String browserSize();
